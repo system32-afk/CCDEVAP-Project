04a2ad785e0bfa64f384c9fd9bb4bf86ab70082a
system32-afk <johngabrielsalamera@gmail.com>
2026-06-11 21:32:00 +0800

    modularized searchable dropdown

--- scripts/search_script.js
+++ scripts/search_script.js
@@ -2,10 +2,6 @@
 
 const cityInput = $('.city-search');
 const results = $('.dropdown-list');
-const cities = 
-['NAIA (Manila)', 'BIA (Albay)', 'MCIA (Cebu)', 'CIA (Clark)', 'BSA (Bacolod)','IIA (Iloilo)',
-'SA (Siargao)','GPRA (Caticlan)'
-];
 const departDate = $("#departure-date");
 const returnDate = $("#return-date");
 const booking_info = getBookingInfo();
@@ -66,62 +62,13 @@ returnDate.on("change", function(){
 
 
 
-//hides all dropdown lists when the user clicks outside of the input fields
-$(document).on('click', function(event) {
 
-    if (!$(event.target).closest('.input-fields').length) {
-        $('.dropdown-list').hide();
-    }
-
-});
 
 //makes the input field into a searchable dropbox
 
 cityInput.on('input', function() {
-    var currentSearchField = $(this);
-    var currentResults = currentSearchField.siblings('.dropdown-list');
-    var search = currentSearchField.val().toLowerCase(); //get the value of the input field and convert it to lowercase
-    var showResults = function(){
-        currentResults.show(); //show the dropdown list when the input field is focused
-    };
-    var hideResults = function(){
-        currentResults.hide(); //hide the dropdown list when the input field is blurred
-    };
-    var searchFieldID = currentSearchField.attr('id'); //for booking_info data manipulation
-
-    currentResults.empty(); //clear the dropdown list
-    showResults(); //show the dropdown list
-    
-    /*
-        Okay medyo mahaba to, so basically .filter filters the cities array to only include cities that match what the user is typing
-        tapos it returns a new array that the .foreach iterates through and displays yung mga matching cities na yun
-    */
-    cities.filter(city => city.toLowerCase().includes(search)). //filter the cities array to only include cities that match the search query
-        forEach(city =>{
-
-        var options = $('<div></div>'); //create new div
-        
-        options.text(city); //set the text content of the div to the city name
-        options.addClass('city-item');//name the div class to city-item
-
-        options.click(event =>{
-
-            if(searchFieldID === "origin-city-search"){
-                booking_info.originCity = city;
-            }else if (searchFieldID === "destination-city-search"){
-                 booking_info.destinationCity = city;
-            }
-
-            console.log("origin:"+ booking_info.originCity);
-            console.log("destination:"+booking_info.destinationCity);
-            currentSearchField.val(city);
-            currentResults.empty(); //clear the dropdown list
-            hideResults(); //hide the dropdown list
-        });
-
-        currentResults.append(options); //pasok yung new div sa dropdown list
-    })
-    
+    var inputField = $(this);
+    searchDropdown(inputField,inputField.siblings('.dropdown-list'),inputField.attr('id'))
 });
 
 
---
+++ scripts/utilities/search_dropdown_script.js
@@ -0,0 +1,57 @@
+const cities = 
+['NAIA (Manila)', 'BIA (Albay)', 'MCIA (Cebu)', 'CIA (Clark)', 'BSA (Bacolod)','IIA (Iloilo)',
+'SA (Siargao)','GPRA (Caticlan)'
+];
+
+//hides all dropdown lists when the user clicks outside of the input fields
+$(document).on('click', function(event) {
+
+    if (!$(event.target).closest('.input-fields').length) {
+        $('.dropdown-list').hide();
+    }
+
+});
+
+function searchDropdown (currentSearchField,dropdown,searchFieldID){
+    var search = currentSearchField.val().toLowerCase(); //get the value of the input field and convert it to lowercase
+    var showResults = function(){
+        dropdown.show(); //show the dropdown list when the input field is focused
+    };
+    var hideResults = function(){
+        dropdown.hide(); //hide the dropdown list when the input field is blurred
+    };
+    var searchFieldID = currentSearchField.attr('id'); //for booking_info data manipulation
+
+    dropdown.empty(); //clear the dropdown list
+    showResults(); //show the dropdown list
+    
+    /*
+        Okay medyo mahaba to, so basically .filter filters the cities array to only include cities that match what the user is typing
+        tapos it returns a new array that the .foreach iterates through and displays yung mga matching cities na yun
+    */
+    cities.filter(city => city.toLowerCase().includes(search)). //filter the cities array to only include cities that match the search query
+        forEach(city =>{
+
+        var options = $('<div></div>'); //create new div
+        
+        options.text(city); //set the text content of the div to the city name
+        options.addClass('city-item');//name the div class to city-item
+
+        options.click(event =>{
+
+            if(searchFieldID === "origin-city-search"){
+                booking_info.originCity = city;
+            }else if (searchFieldID === "destination-city-search"){
+                 booking_info.destinationCity = city;
+            }
+
+            console.log("origin:"+ booking_info.originCity);
+            console.log("destination:"+booking_info.destinationCity);
+            currentSearchField.val(city);
+            dropdown.empty(); //clear the dropdown list
+            hideResults(); //hide the dropdown list
+        });
+
+        dropdown.append(options); //pasok yung new div sa dropdown list
+    })
+}
