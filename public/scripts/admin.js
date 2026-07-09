9e2b7e2e6ea9d9036d267a1673ed9d32d40b4b9f
theut321 <jairus_villafranca@dlsu.edu.ph>
2026-07-09 11:47:22 +0800

    working fetch for soft delete but read not yet

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -15,3 +15,32 @@ function updateStatusClass(select) {
     select.classList.remove("confirmed", "pending", "cancelled");
     select.classList.add(select.value);
 }
+
+function applyFilter(){
+    const cabinSelect = document.getElementById('cabinSelect').value();
+
+    fetch(`/api/flights?cabin=${cabin}`)
+}
+
+let selectedFlightNumber = null;
+
+function openCancelModal(flightNumber){
+    selectedFlightNumber = flightNumber;
+    openModal("modal-cancel-flight");
+}
+
+async function confirmDeactivate(){
+
+    const response = await fetch(
+        `/admin-flights/${selectedFlightNumber}/deactivate`,
+        {
+            method: "PATCH"
+        }
+    );
+
+    if(response.ok){
+        location.reload();
+    }else{
+        alert("Unable to cancel flight.");
+    }
+}
